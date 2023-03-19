0f71e4555349dd95b183070c5a08217e32360b44
Andrei_Moanta <andrei.moanta@gmail.com>
2023-03-19 14:55:43 +0200

    Am adaugat alerta pentru depasirea valorilor normale.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -17,7 +18,7 @@ export class HomePage {
   optimal_pr_sis=135;
   optimal_pr_dis=85;
 
-  constructor() {
+  constructor(private alertController: AlertController) {
     this.pr_r_sis=NaN;
     this.pr_r_dis=NaN;
     this.pr_l_sis=NaN;
@@ -28,9 +29,21 @@ export class HomePage {
     return isNaN(this.pr_r_sis) || isNaN(this.pr_r_dis) || isNaN(this.pr_l_sis) || isNaN(this.pr_l_dis)
   }
 
+  async ExcidingValues() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      cssClass: 'ion-text-center',
+      subHeader: 'O nu, valorile tensiuni depasesc limitele normale!',
+      message: 'Incearca sa iei tensiunea inca odata.',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
   SaveBloodPreasure(){
     if(this.pr_r_sis > this.optimal_pr_sis || this.pr_l_sis > this.optimal_pr_sis ||this.pr_r_dis > this.optimal_pr_dis || this.pr_l_dis > this.optimal_pr_dis){
-      console.log("I have to add an alert for exeding normal values");
+      this.ExcidingValues();
     }else{
       console.log("I have to redirect the user to the congratulations page");
     }
