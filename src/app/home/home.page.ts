c1b67fddd1c0470b274e04b34f8dece5dfe3c24f
Andrei_Moanta <andrei.moanta@gmail.com>
2023-03-19 14:19:30 +0200

    Am adaugat preluarea volorilor tensiunii. In cazul in care nu sunt completate toate campurile nu se permite salvarea.
    In acest moment pentru a vedea incadrarea in limite a valorilor este afisat un text.
    
    Am mai facut cateva modifiacri marunte in ceea ce priveste designul pagini.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  // blood preasure for left and right hand sistolical and diastolical values
+  pr_r_sis: number;
+  pr_r_dis: number;
+  pr_l_sis: number;
+  pr_l_dis: number;
 
-  constructor() {}
+  // blood preasure normal values
+  optimal_pr_sis=135;
+  optimal_pr_dis=85;
+
+  constructor() {
+    this.pr_r_sis=NaN;
+    this.pr_r_dis=NaN;
+    this.pr_l_sis=NaN;
+    this.pr_l_dis=NaN;
+  }
+
+  AllValuesExisting(){
+    return isNaN(this.pr_r_sis) || isNaN(this.pr_r_dis) || isNaN(this.pr_l_sis) || isNaN(this.pr_l_dis)
+  }
+
+  SaveBloodPreasure(){
+    if(this.pr_r_sis > this.optimal_pr_sis || this.pr_l_sis > this.optimal_pr_sis ||this.pr_r_dis > this.optimal_pr_dis || this.pr_l_dis > this.optimal_pr_dis){
+      console.log("I have to add an alert for exeding normal values");
+    }else{
+      console.log("I have to redirect the user to the congratulations page");
+    }
+  }
 
 }
